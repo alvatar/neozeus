d5507989c0960cd9a23b56068b6fca13fd1f4e00
Álvaro Castro Castilla <alvatar@proton.me>
2026-03-28 20:51:39 +0200

    refactor: move hud test exports into sibling file

---
+++ src/hud/tests.rs
@@ -0,0 +1,17 @@
+pub(crate) use super::compositor::setup_hud_offscreen_compositor;
+pub(crate) use super::compositor::{
+    HudCompositeLayerId, HudCompositeLayerMarker, HUD_COMPOSITE_FOREGROUND_Z,
+    HUD_COMPOSITE_RENDER_LAYER,
+};
+pub(crate) use super::modules::{
+    agent_row_rect, agent_rows, debug_toolbar_buttons, handle_pointer_click, handle_scroll,
+    AgentListRowSection,
+};
+pub(crate) use super::render::{
+    HudVectorSceneMarker, HUD_MODAL_CAMERA_ORDER, HUD_MODAL_RENDER_LAYER,
+};
+pub(crate) use super::state::{
+    default_hud_module_instance, docked_agent_list_rect, HudDragState, HudModalState, HudState,
+};
+pub(crate) use super::view_models::{AgentListRowView, ConversationListRowView};
+pub(crate) use super::widgets::HUD_WIDGET_DEFINITIONS as HUD_MODULE_DEFINITIONS;
