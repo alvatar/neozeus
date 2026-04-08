e187b6c3458cffa53977c0edb4ec32e0d7e5d0a9
Álvaro Castro Castilla <alvatar@proton.me>
2026-04-08 15:17:40 +0300

    Remove orphan widget use case file

--- src/app/use_cases/widget.rs
+++
@@ -1,6 +0,0 @@
-use crate::hud::{HudLayoutState, HudWidgetKey};
-
-/// Resets widget.
-pub(crate) fn reset_widget(widget_id: HudWidgetKey, layout_state: &mut HudLayoutState) {
-    layout_state.reset_module(widget_id);
-}
